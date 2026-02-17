9c2fc50c97ceec84d879b0ba6815fd7910f7c185
Niladri Sekhar Mondal <115944239+niladrisekhar8712@users.noreply.github.com>
2026-02-17 19:47:33 +0530

    Add files via upload

---
+++ Chapter_24/vfork.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/* demonstration that closing an FD by child process does not
+affect the corresponding FD in the parent*/
+int errExit(char *msg)
+{
+    printf("%s", msg);
+    exit(EXIT_FAILURE);
+}
+int main(int argc, char *argv[])
+{
+    int filePrem = S_IRUSR | S_IWUSR;
+    int fd = open("test1.txt", O_TRUNC | O_CREAT | O_RDWR, filePrem);
+
+    int childpid;
+    switch (childpid = vfork())
+    {
+    case -1:
+        errExit("Child process creation failure\n");
+    case 0:
+        if (close(fd) == -1)
+        {
+            errExit("Fd closure failure\n");
+        }
+
+        printf("FD closed by child process\n");
+        exit(EXIT_SUCCESS);
+    default:
+        break;
+    }
+    int f;
+    if ((f = write(fd, "abcd", 2)) != -1)
+    {
+        printf("Successfully written %d bytes to fd\n", f);
+        if (close(f) == -1)
+        {
+            errExit("There some error occured from write\n");
+        }
+        else
+            printf("Confirmed that closing a fd opend by child is not affected the corresponding fd in parent\n");
+    }
+    else
+    {
+        errExit("There some error occured\n");
+    }
+    exit(EXIT_SUCCESS);
+}
